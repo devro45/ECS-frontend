c2af47ef2f30a4d34e3ca3ffc58ff762808e346e
Dev <devro3014@gmail.com>
2023-02-22 09:42:32 +0530

    first commit

---
+++ src/firebaseBaseConfig.js
@@ -0,0 +1,19 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAwR_WCLK2e_x5NK_I2yCdSNnJQC1rPxL8",
+//     authDomain: "easy-consulting-react.firebaseapp.com",
+//     projectId: "easy-consulting-react",
+//     storageBucket: "easy-consulting-react.appspot.com",
+//     messagingSenderId: "471032014480",
+//     appId: "1:471032014480:web:cbd7dc3d102ddd71d55a15"
+//   };export default firebaseConfig;
+const firebaseConfig = {
+    apiKey: "AIzaSyDYFi5NSmUU5yCJesu1DPz1it031fEWWDY",
+    authDomain: "fir-project-373d1.firebaseapp.com",
+    projectId: "fir-project-373d1",
+    storageBucket: "fir-project-373d1.appspot.com",
+    messagingSenderId: "166938384892",
+    appId: "1:166938384892:web:ad5991254e5a34b8d5fa79",
+    measurementId: "G-2VXB2VQLPW"
+};
+
+export default firebaseConfig;
